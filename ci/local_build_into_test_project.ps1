77188cada10ce9b07eb0053255ced8f74b5aba22
kalle (jag) <kalle.jillheden@gmail.com>
2019-12-30 21:28:41 +0100

    Added simple local_build_into_test_project utility script

---
+++ ci/local_build_into_test_project.ps1
@@ -0,0 +1,15 @@
+
+Write-Host ""
+Write-Host ">>> BUILDING DEBUG BUILD OF PACKAGE USING local_build_into_package.ps1 " -BackgroundColor DarkCyan -ForegroundColor White
+Write-Host ""
+&$PSScriptRoot\local_build_into_package.ps1 `
+    -Configuration Debug `
+    -UnityBuilds @('Tests') `
+    -RelativeBuildDestinationBase "Src/Newtonsoft.Json-for-Unity.Tests/Packages/Newtonsoft.Json-for-Unity.Tests/Plugins/" `
+    -DontSign
+if ($LASTEXITCODE -ne 0) {
+    throw "Failed to complete debug build"
+}
+Write-Host ""
+Write-Host ">>> COMPLETED DEBUG BUILD OF PACKAGE USING local_build_into_package.ps1 " -BackgroundColor DarkGreen -ForegroundColor White
+Write-Host ""
